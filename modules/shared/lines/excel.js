34c6d76a7d053c5a547f41208087affd2af2c8c2
✦𝐏𝐎𝐊✦ŦHΣ🦎δΛŁΛMΛÑÐΣR <dad2lna@gmail.com>
2026-09-25 12:40:44 -0500

    feat(cert): add Cert pool column to lines Excel

--- modules/shared/lines/excel.js
+++ modules/shared/lines/excel.js
@@ -152,7 +152,7 @@ function generateAndDownloadXlsx(S) {
   var tmMap = teamColorMap(S, lines);
   var days = 7;
   var dayNames = S.DAYS || ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  var metaHeaders = ["Team", "Line", "Shift", "Start", "End", "Position", "Emp", "Sex", "Function", "RDOs", "Paid"];
+  var metaHeaders = ["Team", "Line", "Shift", "Start", "End", "Position", "Emp", "Sex", "Function", "Cert pool", "RDOs", "Paid"];
   var headers = metaHeaders.concat(dayNames, ["Hours"]);
   var workbook = new ExcelJS.Workbook();
   workbook.creator = "BrokeSched";
@@ -182,7 +182,7 @@ function generateAndDownloadXlsx(S) {
     tableRows.push([
       teamName, line.lineCode || "", line.shiftName || (sh && sh.name) || "",
       sh ? sh.start : "", sh ? sh.end : "", exportPosition(line), exportEmpClass(line),
-      line.sex || "", line.function || "", rdo, line.paid || ""
+      line.sex || "", line.function || "", line.certPool || "", rdo, line.paid || ""
     ].concat(dayValues, [hours]));
     rowMeta.push({ days: dayFlags, teamFill: teamName ? tmMap[teamName] : null });
   });
@@ -232,7 +232,7 @@ function generateAndDownloadXlsx(S) {
     }
   }
   sheet.columns.forEach(function (col, idx) {
-    col.width = idx === 0 ? 10 : idx < 11 ? 12 : 11;
+    col.width = idx === 0 ? 10 : idx < 12 ? 12 : 11;
   });
   workbook.xlsx.writeBuffer().then(function (buf) {
     var blob = new Blob([buf], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
